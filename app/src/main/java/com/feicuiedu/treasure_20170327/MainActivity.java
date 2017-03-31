1c82b8996604e1ca257d542b06a505e9a39bb608
gqq <gqq519@163.com>
2017-03-31 16:44:08 +0800

    Home页面侧滑处理及个人信息展示

--- app/src/main/java/com/feicuiedu/treasure_20170327/MainActivity.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.feicuiedu.treasure_20170327.commons.ActivityUtils;
+import com.feicuiedu.treasure_20170327.treasure.HomeActivity;
+import com.feicuiedu.treasure_20170327.user.UserPrefs;
 import com.feicuiedu.treasure_20170327.user.login.LoginActivity;
 import com.feicuiedu.treasure_20170327.user.register.RegisterActivity;
 
@@ -38,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mActivityUtils = new ActivityUtils(this);
         mUnbinder = ButterKnife.bind(this);
+
+        // 判断一下用户是不是已经登录过了
+        SharedPreferences preferences = getSharedPreferences("user_info", MODE_PRIVATE);
+        if (preferences!=null){
+
+            // 如果登录了
+            if (preferences.getInt("key_tokenid",0)== UserPrefs.getInstance().getTokenid()){
+                mActivityUtils.startActivity(HomeActivity.class);
+                finish();
+            }
+        }
 
         // 注册本地广播
         IntentFilter fliter = new IntentFilter(MAIN_ACTION);
--- app/src/main/java/com/feicuiedu/treasure_20170327/treasure/HomeActivity.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/treasure/HomeActivity.java
@@ -1,15 +1,100 @@
 package com.feicuiedu.treasure_20170327.treasure;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.feicuiedu.treasure_20170327.R;
+import com.feicuiedu.treasure_20170327.user.UserPrefs;
+import com.squareup.picasso.Picasso;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation)
+    NavigationView mNavigationView;
+    private ImageView mIvIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
+
+        // toolbar
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar()!=null){
+            // 不显示默认的标题，而是显示布局中自己加的TextView
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        // DrawerLayout的侧滑监听
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        toggle.syncState();// 同步状态
+        mDrawerLayout.addDrawerListener(toggle);
+
+        // 侧滑菜单item的选择监听
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        // 找到侧滑的头布局里面的头像
+        mIvIcon = (ImageView) mNavigationView.getHeaderView(0).findViewById(R.id.iv_usericon);
+        mIvIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "头像", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // 更新头像信息
+        String photo = UserPrefs.getInstance().getPhoto();
+        if (photo!=null){
+            // 加载头像：采用Picasso实现
+            Picasso
+                    .with(this)
+                    .load(photo)
+                    .into(mIvIcon);
+        }
+    }
+
+    // 侧滑的Navigation的Item每一项被选择的时候会触发
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_hide:// 埋藏宝藏的时候
+                Toast.makeText(this, "埋藏宝藏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_logout:// 退出的时候
+                Toast.makeText(this, "退出", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
