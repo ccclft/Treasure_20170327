0f5d061efd8a000036707c0adae8c6bba8ceae05
gqq <gqq519@163.com>
2017-04-01 11:24:12 +0800

    百度地图集成与展示

--- app/src/main/java/com/feicuiedu/treasure_20170327/TreasureApplication.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/TreasureApplication.java
@@ -2,6 +2,7 @@ package com.feicuiedu.treasure_20170327;
 
 import android.app.Application;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.feicuiedu.treasure_20170327.user.UserPrefs;
 
 /**
@@ -16,6 +17,11 @@ public class TreasureApplication extends Application{
 
         // 用户仓库的初始化
         UserPrefs.init(getApplicationContext());
+
+        // 百度地图SDK的初始化
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
 
     }
 }
