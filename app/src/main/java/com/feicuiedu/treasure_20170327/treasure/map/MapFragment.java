2d69ab824f14fd3f756582b098879bd546aa7d7f
gqq <gqq519@163.com>
2017-04-01 14:51:41 +0800

    地图的创建和展示

--- app/src/main/java/com/feicuiedu/treasure_20170327/treasure/map/MapFragment.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/treasure/map/MapFragment.java
@@ -3,22 +3,112 @@ package com.feicuiedu.treasure_20170327.treasure.map;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapView;
 import com.feicuiedu.treasure_20170327.R;
+
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by gqq on 2017/3/31.
  */
 // 地图和宝藏的展示
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment {
+
+    @BindView(R.id.iv_located)
+    ImageView mIvLocated;
+    @BindView(R.id.btn_HideHere)
+    Button mBtnHideHere;
+    @BindView(R.id.centerLayout)
+    RelativeLayout mCenterLayout;
+    @BindView(R.id.iv_scaleUp)
+    ImageView mIvScaleUp;
+    @BindView(R.id.iv_scaleDown)
+    ImageView mIvScaleDown;
+    @BindView(R.id.tv_located)
+    TextView mTvLocated;
+    @BindView(R.id.tv_satellite)
+    TextView mTvSatellite;
+    @BindView(R.id.tv_compass)
+    TextView mTvCompass;
+    @BindView(R.id.ll_locationBar)
+    LinearLayout mLlLocationBar;
+    @BindView(R.id.tv_currentLocation)
+    TextView mTvCurrentLocation;
+    @BindView(R.id.iv_toTreasureInfo)
+    ImageView mIvToTreasureInfo;
+    @BindView(R.id.et_treasureTitle)
+    EditText mEtTreasureTitle;
+    @BindView(R.id.cardView)
+    CardView mCardView;
+    @BindView(R.id.layout_bottom)
+    FrameLayout mLayoutBottom;
+    @BindView(R.id.map_frame)
+    FrameLayout mMapFrame;
+    private BaiduMap mBaiduMap;
+    private MapView mMapView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map,container);
+        View view = inflater.inflate(R.layout.fragment_map, container);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ButterKnife.bind(this, view);
+        // 视图的处理工作
+
+        initMapView();
+
+    }
+
+    // 初始化百度地图的操作
+    private void initMapView() {
+
+        // 地图的状态
+        MapStatus mapstatus = new MapStatus.Builder()
+                .rotate(0)// 旋转的角度
+                .zoom(15)// 默认是12，范围3-21
+                .overlook(0)// 俯仰的角度
+                .build();
+
+        // 设置地图的信息
+        BaiduMapOptions options = new BaiduMapOptions()
+                .mapStatus(mapstatus)
+                .compassEnabled(true)// 是否显示指南针，默认显示
+                .zoomGesturesEnabled(true)// 是否允许缩放手势
+                .scaleControlEnabled(false)// 不显示比例尺
+                .zoomControlsEnabled(false)// 不显示缩放的控件
+                ;
+
+        // 创建地图控件
+        mMapView = new MapView(getContext(), options);
+
+        // 在布局中添加地图的控件：0，放置在第一位
+        mMapFrame.addView(mMapView,0);
+
+        // 拿到地图的操作类(设置地图的视图、地图状态变化、添加覆盖物等)
+        mBaiduMap = mMapView.getMap();
     }
 }
