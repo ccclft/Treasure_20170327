b4ddb0f0a6fb1eb44cb56f7881c0686dcf228dec
gqq <gqq519@163.com>
2017-04-01 16:32:57 +0800

    地图的定位实现

--- app/src/main/java/com/feicuiedu/treasure_20170327/treasure/map/MapFragment.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/treasure/map/MapFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +16,18 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.feicuiedu.treasure_20170327.R;
 
 import java.util.Map;
@@ -67,6 +74,7 @@ public class MapFragment extends Fragment {
     FrameLayout mMapFrame;
     private BaiduMap mBaiduMap;
     private MapView mMapView;
+    private LocationClient mLocationClient;
 
     @Nullable
     @Override
@@ -82,9 +90,76 @@ public class MapFragment extends Fragment {
         ButterKnife.bind(this, view);
         // 视图的处理工作
 
+        // 初始化百度地图
         initMapView();
 
+        // 初始化定位相关
+        initLocation();
+
     }
+
+    // 初始化定位相关
+    private void initLocation() {
+
+        // 前置：激活定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+
+        // 1. 第一步，初始化LocationClient类
+        mLocationClient = new LocationClient(getContext().getApplicationContext());
+
+        // 2. 第二步，配置定位SDK参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// 打开GPS
+        option.setCoorType("bd09ll");// 设置坐标类型，默认gcj02，会有偏差，设置返回的定位结果坐标系
+        option.setIsNeedAddress(true);// 需要地址信息
+        // 设置参数给LocationClient
+        mLocationClient.setLocOption(option);
+
+        // 3. 第三步，实现BDLocationListener接口
+        mLocationClient.registerLocationListener(mBDLocationListener);
+
+        // 4. 第四步，开始定位
+        mLocationClient.start();
+
+    }
+
+    // 定位监听
+    private BDLocationListener mBDLocationListener = new BDLocationListener() {
+
+        // 当获取到定位数据的时候会触发
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+
+            if (bdLocation==null){
+                // 没有拿到数据，可以重新进行请求
+                mLocationClient.requestLocation();
+                return;
+            }
+
+            // 拿到定位的经纬度
+            double latitude = bdLocation.getLatitude();
+            double longitude = bdLocation.getLongitude();
+
+            // 定位的位置和地址
+            LatLng currentLocation = new LatLng(latitude,longitude);
+            String currentAddr = bdLocation.getAddrStr();
+
+            Log.i("TAG","定位的位置："+currentAddr+"经纬度："+latitude+","+longitude);
+
+            // 地图上设置定位数据
+            MyLocationData locationData = new MyLocationData.Builder()
+                    // 设置定位的经纬度
+                    .latitude(latitude)
+                    .longitude(longitude)
+                    .accuracy(100f)// 定位精度的大小
+                    .build();
+
+            mBaiduMap.setMyLocationData(locationData);
+
+            // TODO 将地图移动到定位的位置
+
+        }
+    };
 
     // 初始化百度地图的操作
     private void initMapView() {
