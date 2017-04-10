6dfeeade92a1dcdf7ce7f0dad61fcd68897e3e43
gqq <gqq519@163.com>
2017-04-10 14:44:58 +0800

    个人信息页面的自定义弹窗处理

--- app/src/main/java/com/feicuiedu/treasure_20170327/user/account/AccountActivity.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/user/account/AccountActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import com.feicuiedu.treasure_20170327.R;
 import com.feicuiedu.treasure_20170327.commons.ActivityUtils;
+import com.feicuiedu.treasure_20170327.custom.IconSelectWindow;
 import com.feicuiedu.treasure_20170327.user.UserPrefs;
 import com.squareup.picasso.Picasso;
 
@@ -25,6 +26,7 @@ public class AccountActivity extends AppCompatActivity {
     ImageView mIVIcon;
 
     private ActivityUtils mActivityUtils;
+    private IconSelectWindow mSelectWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,9 +55,30 @@ public class AccountActivity extends AppCompatActivity {
     // 点击头像：弹出一个弹窗(自定义一个PopupWindow)
     @OnClick(R.id.iv_usericon)
     public void showPhotoWindow(){
-        // 吐司
-        mActivityUtils.showToast("点击了头像");
+        if (mSelectWindow==null){
+            mSelectWindow = new IconSelectWindow(this,mListener);
+        }
+        if (mSelectWindow.isShowing()){
+            mSelectWindow.dismiss();
+            return;
+        }
+        mSelectWindow.show();
     }
+
+    // 跳转的监听
+    private IconSelectWindow.Listener mListener = new IconSelectWindow.Listener() {
+        // 到相册
+        @Override
+        public void toGallery() {
+            mActivityUtils.showToast("相册");
+        }
+
+        // 到相机
+        @Override
+        public void toCamera() {
+            mActivityUtils.showToast("相机");
+        }
+    };
 
     // 返回箭头的处理
     @Override
