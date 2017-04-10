52351d04788a92fef95a6bda4e61b26a6d1a9a4f
gqq <gqq519@163.com>
2017-04-10 15:49:30 +0800

    头像跳转及剪切处理

--- app/src/main/java/com/feicuiedu/treasure_20170327/user/account/AccountActivity.java
+++ app/src/main/java/com/feicuiedu/treasure_20170327/user/account/AccountActivity.java
@@ -1,5 +1,8 @@
 package com.feicuiedu.treasure_20170327.user.account;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +15,12 @@ import com.feicuiedu.treasure_20170327.commons.ActivityUtils;
 import com.feicuiedu.treasure_20170327.custom.IconSelectWindow;
 import com.feicuiedu.treasure_20170327.user.UserPrefs;
 import com.squareup.picasso.Picasso;
+
+import org.hybridsquad.android.library.CropHandler;
+import org.hybridsquad.android.library.CropHelper;
+import org.hybridsquad.android.library.CropParams;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -70,15 +79,76 @@ public class AccountActivity extends AppCompatActivity {
         // 到相册
         @Override
         public void toGallery() {
-            mActivityUtils.showToast("相册");
+            // 清除缓存
+            CropHelper.clearCachedCropFile(mCropHandler.getCropParams().uri);
+
+            Intent intent = CropHelper.buildCropFromGalleryIntent(mCropHandler.getCropParams());
+            startActivityForResult(intent, CropHelper.REQUEST_CROP);
         }
 
         // 到相机
         @Override
         public void toCamera() {
-            mActivityUtils.showToast("相机");
+            // 清除之前剪切的图片的缓存
+            CropHelper.clearCachedCropFile(mCropHandler.getCropParams().uri);
+
+            // 跳转
+            Intent intent = CropHelper.buildCaptureIntent(mCropHandler.getCropParams().uri);
+            startActivityForResult(intent, CropHelper.REQUEST_CAMERA);
         }
     };
+
+    // 图片处理
+    private CropHandler mCropHandler = new CropHandler() {
+        // 图片剪切之后：参数Uri代表剪切后的图片
+        @Override
+        public void onPhotoCropped(Uri uri) {
+            // 拿到剪切之后的图片
+            File file = new File(uri.getPath());
+            // TODO 进行网络请求将图片上传
+            mActivityUtils.showToast(uri.getPath());
+        }
+
+        // 取消
+        @Override
+        public void onCropCancel() {
+            mActivityUtils.showToast("剪切取消");
+        }
+
+        // 剪切失败
+        @Override
+        public void onCropFailed(String message) {
+            mActivityUtils.showToast(message);
+        }
+
+        // 剪切的参数设置：Uri(图片剪切之后保存的路径)
+        @Override
+        public CropParams getCropParams() {
+            // 默认的剪切设置
+            CropParams cropParams = new CropParams();
+            return cropParams;
+        }
+
+        // 上下文
+        @Override
+        public Activity getContext() {
+            return AccountActivity.this;
+        }
+    };
+
+    // 处理图片剪切的结果
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        CropHelper.handleResult(mCropHandler,requestCode,resultCode,data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mCropHandler.getCropParams() != null)
+            CropHelper.clearCachedCropFile(mCropHandler.getCropParams().uri);
+        super.onDestroy();
+    }
 
     // 返回箭头的处理
     @Override
